a2d000ab88ba513274389785dea6d3be49ccf158
wsyxl365 <771388996@qq.com>
2018-10-14 21:50:27 +0800

    设置忽略和服务配置

---
+++ js-object/webpack.dev.config.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Administrator on 2018/9/4 0004.
+ */
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: __dirname,
+		filename: './release/bundle.js'
+	},
+	module: {
+		rules: [{
+			test: /\.js?$/,
+			exclude: /(node_modules)/,
+			loader: "babel-loader"
+		}]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./index.html"
+		})
+	],
+	devServer: {
+		contentBase: path.join(__dirname, './release'), //根目录
+		open: true,
+		port: 9000
+	}
+}
